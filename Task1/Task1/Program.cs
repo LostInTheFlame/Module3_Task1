a11d018cb1e3b3495061474f207df2c1589d5db2
Maksim <xSeptentrioNx@gmail.com>
2020-02-03 18:17:45 +0300

    Version 1.0

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            string A = Console.ReadLine();
+            if (int.TryParse(A, out int a))
+            {
+                Console.Write("Введите второе число: ");
+                string B = Console.ReadLine();
+                if (int.TryParse(B, out int b))
+                {
+                    int resault = 0;
+                    for (int i = 1; i <= b; i++)
+                    {
+                        resault = resault + a;
+                    }
+                    Console.WriteLine($"a * b = {resault}");
+                }
+                else Console.WriteLine("Значение введено неверно.");
+            }
+            else Console.WriteLine("Значение введено неверно.");
+            Console.ReadKey();
+        }
+    }
+}
